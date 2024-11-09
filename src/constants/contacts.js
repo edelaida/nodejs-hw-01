aa57287ad19bf1364265d58a1a3af3fa019c5976
edelaida <edelaida@gmail.com>
2024-11-09 10:42:23 +0200

    node222

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+export const PATH_DB = path.resolve('src/db/db.json')
+
+// import path from 'node:path';
+// import * as fs from 'node:fs/promises';
+
+// const FILE_PATH = path.resolve('db.json');
