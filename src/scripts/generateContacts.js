9a25076807cacd4e562f18819026c75f3ac0b70d
edelaida <edelaida@gmail.com>
2024-11-10 19:06:09 +0200

    remove-last

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,14 +1,16 @@
 import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
 
 const generateContacts = async (number) => { 
-    const data = await fs.readFile(PATH_DB);
-    const parsData = JSON.parse(data);
+  const data = await readContacts();
+    // const data = await fs.readFile(PATH_DB);
+    // const parsData = JSON.parse(data);
     for (let i = 0; i < number; i++) {
-        parsData.push(createFakeContact())
+        data.push(createFakeContact())
       }
-      await fs.writeFile(PATH_DB, JSON.stringify(parsData, null, 2), 'utf-8');
+      await fs.writeFile(PATH_DB, JSON.stringify(data, null, 2), 'utf-8');
   };
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,16 +1,15 @@
 import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeLastContact = async () => {
     const data = await fs.readFile(PATH_DB);
     const allData = JSON.parse(data);
-    const number = allData.length;
-       
-    if (number > 0) {
-        number-=1;
-        allData.slice(0, number);
+    let number = allData.length;
+        if (number > 0) {
+        number -= 1;
+      await writeContacts(allData.slice(0, number));
     }
-      await fs.writeFile(PATH_DB, JSON.stringify(allData, null, 2), 'utf-8');
  };
 
 removeLastContact();
